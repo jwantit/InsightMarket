54d997137bd6a583d49a34e1b10349982c393244
Kimjiwon <kgw072089@gmail.com>
2025-12-19 18:30:43 +0900

    Init Project

---
+++ src/test/java/com/InsightMarket/InsightMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InsightMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
