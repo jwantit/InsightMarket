13c3b52bf747d47c71131d4a1ffba1b87172801e
Kimjiwon <kgw072089@gmail.com>
2025-12-19 18:30:43 +0900

    Init Project

---
+++ src/main/java/com/InsightMarket/InsightMarketApplication.java
@@ -0,0 +1,13 @@
+package com.InsightMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightMarketApplication.class, args);
+	}
+
+}
